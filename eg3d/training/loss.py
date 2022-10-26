5b2760abed130003108d6c246160df00e1f51f98
mengxuyiGit <mengxuyiabc@gmail.com>
2022-10-26 02:44:31 -0700

    removed centerCrop in processing perception loss input

--- eg3d/training/loss.py
+++ eg3d/training/loss.py
@@ -76,7 +76,7 @@ class StyleGAN2Loss(Loss):
             n_px = 224
             self.my_process = Compose([
                     Resize(n_px, interpolation=InterpolationMode.BICUBIC),
-                    CenterCrop(n_px),
+                    # CenterCrop(n_px), # unnecessary
                     Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
                 ])
 
