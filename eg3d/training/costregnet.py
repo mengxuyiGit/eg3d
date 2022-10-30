b734b1576de695544d34e930abb32037c707c2dd
mengxuyiGit <mengxuyiabc@gmail.com>
2022-10-30 02:00:45 -0700

    rewrite modulation process

--- eg3d/training/costregnet.py
+++ eg3d/training/costregnet.py
@@ -22,6 +22,29 @@ class ConvBnReLU3D(nn.Module):
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
 
+def calc_mean_std(feat, eps=1e-5):
+    # eps is a small value added to the variance to avoid divide-by-zero.
+    size = feat.size()
+    assert (len(size) == 5)
+    N, C = size[:2]
+    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(N, C, 1, 1, 1)
+    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1, 1)
+    return feat_mean, feat_std
+
+class ConvInReLU3D(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=3, stride=1, pad=1,
+                 norm_act='IN'):
+        super(ConvInReLU3D, self).__init__()
+        self.conv = nn.Conv3d(in_channels, out_channels,
+                              kernel_size, stride=stride, padding=pad, bias=False)
+        self.inst_norm = nn.InstanceNorm3d(out_channels) # without learnable parameters  
+        self.act = nn.LeakyReLU()
+
+    def forward(self, x):
+        return self.act(self.inst_norm(self.conv(x)))
+
 class CostRegNet_Deeper(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
     def __init__(self, in_channels, out_dim=8, norm_act=InPlaceABN):
         super(CostRegNet_Deeper, self).__init__()
@@ -254,7 +277,7 @@ class PcWsUnet(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
         return res_feature
 
 
-class Synthesis3DUnet(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
+class Synthesis3DUnet_v0(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
     def __init__(self, 
             in_channels, 
             out_dim=8, 
@@ -426,5 +449,177 @@ class Synthesis3DUnet(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
         x = x*style
 
         # print(f"Totally used up to {w_idx} ws in synthesis3DUnet") ## currently used only 5
+        
+        return x
+
+class Synthesis3DUnet(nn.Module): # 256^3 -> 8^3; 128^3 -> 4^3
+    def __init__(self, 
+            in_channels, 
+            out_dim=8, 
+            use_noise=False,
+            noise_strength = 0.5,
+            ws_channel=512,
+            affine_act='relu', #### ???? FIXME: is this a good activation 
+            norm_act=nn.InstanceNorm3d):
+
+        super(Synthesis3DUnet, self).__init__()
+
+        self.use_noise = use_noise
+        # noise_strength = 0.5
+        self.noise_strength = noise_strength
+
+        self.conv0 = ConvInReLU3D(in_channels, out_dim, norm_act=norm_act)
+
+        self.conv1 = ConvInReLU3D(out_dim, 16, stride=2, norm_act=norm_act)
+        self.conv2 = ConvInReLU3D(16, 16, norm_act=norm_act)
+
+        self.conv3 = ConvInReLU3D(16, 32, stride=2, norm_act=norm_act)
+        self.conv4 = ConvInReLU3D(32, 32, norm_act=norm_act)
+
+        self.conv5 = ConvInReLU3D(32, 64, stride=2, norm_act=norm_act)
+        self.conv6 = ConvInReLU3D(64, 64, norm_act=norm_act)
+
+        self.conv51 = ConvInReLU3D(64, 64, stride=2, norm_act=norm_act)
+        self.conv61 = ConvInReLU3D(64, 64, norm_act=norm_act)
+
+        self.conv52 = ConvInReLU3D(64, 64, stride=2, norm_act=norm_act)
+        self.conv62 = ConvInReLU3D(64, 64, norm_act=norm_act)
+
+        self.conv27 = nn.Sequential(
+            nn.ConvTranspose3d(64, 64, 3, padding=1, output_padding=1,
+                               stride=2, bias=False),
+            norm_act(64))
+        self.affine27 = nn.Sequential(
+                        nn.Linear(ws_channel, 64*2),
+                        nn.ReLU()
+                    )
+        
+        
+        self.conv17 = nn.Sequential(
+            nn.ConvTranspose3d(64, 64, 3, padding=1, output_padding=1,
+                               stride=2, bias=False),
+            norm_act(64))
+        self.affine17 = nn.Sequential(
+                        nn.Linear(ws_channel, 64*2),
+                        nn.ReLU()
+                    )
+
+        self.conv7 = nn.Sequential(
+            nn.ConvTranspose3d(64, 32, 3, padding=1, output_padding=1,
+                               stride=2, bias=False),
+            norm_act(32))
+        self.affine7 = nn.Sequential(
+                        nn.Linear(ws_channel, 32*2),
+                        nn.ReLU()
+                    )
+
+        self.conv9 = nn.Sequential(
+            nn.ConvTranspose3d(32, 16, 3, padding=1, output_padding=1,
+                               stride=2, bias=False),
+            norm_act(16))
+        self.affine9 = nn.Sequential(
+                        nn.Linear(ws_channel, 16*2),
+                        nn.ReLU()
+                    )
+
+        # self.conv11 = nn.Sequential(
+        #     nn.ConvTranspose3d(16, 8, 3, padding=1, output_padding=1,
+        #                        stride=2, bias=False),
+        #     norm_act(8))
+        self.conv11 = nn.Sequential(
+            nn.ConvTranspose3d(16, out_dim, 3, padding=1, output_padding=1,
+                               stride=2, bias=False),
+            norm_act(out_dim))
+        self.affine11 = nn.Sequential(
+                        nn.Linear(ws_channel, out_dim*2),
+                        nn.ReLU()
+                    )
+        
+
+        # self.conv12 = nn.Conv3d(8, 8, 3, stride=1, padding=1, bias=True)
+
+    def forward(self, x, ws):
+        conv0 = self.conv0(x)
+        
+
+        conv2 = self.conv2(self.conv1(conv0))
+        conv4 = self.conv4(self.conv3(conv2))
+     
+        conv6 = self.conv6(self.conv5(conv4))
+
+        conv61 = self.conv61(self.conv51(conv6))
+        conv62 = self.conv62(self.conv52(conv61))
+
+        # print("CostRegNetDeeper bottleneck:", conv62.shape) # 256^3 -> 8^3; 128^3 -> 4^3
+        # print("IN 3D conv")
+
+        ### below is upconv process: add noises and latent
+        ## norm -> mod -> noise
+        w_idx=0
+
+        # 1. x is the sum of instance-normed feature volumes
+        x = conv61 + self.conv27(conv62) 
+        
+        # 2. apply style
+        style = self.affine27(ws.narrow(1, w_idx, 1)).permute(0,2,1) # style.shape: B,C,2
+        w_idx += 1
+        B, C = x.shape[:2]
+        style = style.reshape(B,C,2,1,1,1) # extend to 3D 
+        x = x * (style[:,:,0] + 1) + style[:,:,1]
+        # 3. add noise
+        if self.use_noise:
+            noise = torch.rand(x.shape, device=x.device, dtype=torch.float32)
+            noise = noise*self.noise_strength
+            x = x.add_(noise.to(x.dtype))
+
+
+        x = conv6 + self.conv17(x)
+        # 2. apply style
+        style = self.affine17(ws.narrow(1, w_idx, 1)).permute(0,2,1) # style.shape: B,C,2
+        w_idx += 1
+        B, C = x.shape[:2]
+        style = style.reshape(B,C,2,1,1,1) # extend to 3D 
+        x = x * (style[:,:,0] + 1) + style[:,:,1]
+        # 3. add noise
+        if self.use_noise:
+            noise = torch.rand(x.shape, device=x.device, dtype=torch.float32)
+            noise = noise*self.noise_strength
+            x = x.add_(noise.to(x.dtype))
+
+        x = conv4 + self.conv7(x)
+        # 2. apply style
+        style = self.affine7(ws.narrow(1, w_idx, 1)).permute(0,2,1) # style.shape: B,C,2
+        w_idx += 1
+        B, C = x.shape[:2]
+        style = style.reshape(B,C,2,1,1,1) # extend to 3D 
+        x = x * (style[:,:,0] + 1) + style[:,:,1]
+        # 3. add noise
+        if self.use_noise:
+            noise = torch.rand(x.shape, device=x.device, dtype=torch.float32)
+            noise = noise*self.noise_strength
+            x = x.add_(noise.to(x.dtype))      
+
+        x = conv2 + self.conv9(x)
+        # 2. apply style
+        style = self.affine9(ws.narrow(1, w_idx, 1)).permute(0,2,1) # style.shape: B,C,2
+        w_idx += 1
+        B, C = x.shape[:2]
+        style = style.reshape(B,C,2,1,1,1) # extend to 3D 
+        x = x * (style[:,:,0] + 1) + style[:,:,1]
+        # 3. add noise
+        if self.use_noise:
+            noise = torch.rand(x.shape, device=x.device, dtype=torch.float32)
+            noise = noise*self.noise_strength
+            x = x.add_(noise.to(x.dtype))
+
+        x = conv0 + self.conv11(x)
+        # 2. apply style
+        style = self.affine11(ws.narrow(1, w_idx, 1)).permute(0,2,1) # style.shape: B,C,2
+        w_idx += 1
+        B, C = x.shape[:2]
+        style = style.reshape(B,C,2,1,1,1) # extend to 3D 
+        x = x * (style[:,:,0] + 1) + style[:,:,1]
+       
+        # last layer, no noise anymore
         
         return x
