bbad9dbdc6c3bfa4abe625d57623383eea28d0ec
mengxuyiGit <mengxuyiabc@gmail.com>
2022-10-30 09:32:37 +0000

    test passed of branch mod

--- eg3d/scripts/shapenet_car_finetune.sh
+++ eg3d/scripts/shapenet_car_finetune.sh
@@ -4,6 +4,7 @@ CUDA_VISIBLE_DEVICES=1
 GPUS=2
 BATCH_SIZE=4
 BASE_DIR=/home/xuyi/Repo/eg3d
+BASE_DIR=/xuyi-fast-vol/Repo-fast/eg3d
 
 # ##-------- abo/shapenet with triplane -----------
 # # DATA=${BASE_DIR}/dataset_preprocessing/shapenet_cars/cars_128_copy.zip
@@ -45,3 +46,10 @@ python train.py --outdir=${BASE_DIR}/try-runs --cfg=abo_dataset --data=${DATA} \
   # --use_perception True --perception_reg 1 \
   # --use_l1 True \
   # --use_chamfer True \
+  # python train.py --outdir=${BASE_DIR}/training-runs/synthesis_unet/${SUFFIX} --cfg=abo_dataset --data=${DATA} \
+  # --gpus=${GPUS} --batch=${BATCH_SIZE} --gamma=0.3 \
+  # --backbone volume --volume_res ${VOLUME_RES} --decoder_dim 8 \
+  # --snap 10 --workers 16 \
+  # --use_patch ${PATCH} --use_l1 ${PATCH} --l1_reg 100 \
+  # --discriminator_condition_on_real ${COND} --drop_pixel_ratio ${DROP_PIXEL_RATIO} \
+  # --z_from_pc False 
