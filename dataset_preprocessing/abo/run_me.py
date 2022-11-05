7fd0958438dc020574d7b5bc83ded618cfb58c58
mengxuyiGit <mengxuyiabc@gmail.com>
2022-11-05 00:50:25 -0700

    merged from debug_nonGAN: load projection from datast

--- dataset_preprocessing/abo/run_me.py
+++ dataset_preprocessing/abo/run_me.py
@@ -25,23 +25,35 @@ if __name__ == '__main__':
         # output_dataset_name = 'abo_128_completed.zip'
         # output_dataset_name = 'abo_128_completed_white.zip'
         # output_dataset_name = 'abo_512_completed_white.zip'
-        output_dataset_name = 'debug.zip'
+        # output_dataset_name = 'debug_one_obj_one_view.zip'
+        # output_dataset_name = 'debug_one_obj_100_view.zip'
+
+        split = 'train'
+        pc_fbase = 'pc_1024.csv'
+        # output_dataset_name = f'abo_128_2k_{split}.zip'
+        output_dataset_name = f'debug_proj_{split}.zip'
+        
+
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        # extracted_data_path = os.path.join(working_dir, os.path.splitext(download_name)[0])
-        extracted_data_path = '/home/xuyi/Data/renderer/output_abo'
-        print("Downloading data...")
-        # zipped_dataset = os.path.join(working_dir, download_name)
-        # gdown.download(url, zipped_dataset, quiet=False)
+        # extracted_data_path = '/home/xuyi/Data/renderer/output_debug'
+        extracted_data_path = '/home/xuyi/Data/renderer/output_debug'
+        # extracted_data_path = '/home/xuyi/sda/jialin/abo_out/'
 
-        print("Unzipping downloaded data...")
-        # shutil.unpack_archive(zipped_dataset, working_dir)
-
+        
         print("Converting camera parameters...")
-        cmd = f"python {os.path.join(dir_path, 'preprocess_abo_cameras.py')} --source={extracted_data_path}"
+        cmd = f"python {os.path.join(dir_path, 'preprocess_abo_cameras.py')} --source={extracted_data_path} --split={split} --pc_fbase={pc_fbase} "
+        # if os.geteuid() != 0:
+        #     # os.execvp('sudo', ['sudo', cmd])
+        #     subprocess.run(['sudo', cmd], shell=True)
+        # else:
         subprocess.run([cmd], shell=True)
 
         print("Creating dataset zip...")
         cmd = f"python {os.path.join(dir_path, '../../eg3d', 'dataset_tool.py')}"
+<<<<<<< HEAD
         cmd += f" --source {extracted_data_path} --dest {output_dataset_name} --resolution 512x512 --read_pointcloud"
+=======
+        cmd += f" --source {extracted_data_path} --dest {output_dataset_name} --resolution 128x128 --read_pointcloud --split={split}"
+>>>>>>> 4104a3a... added projection in the dataseet
         subprocess.run([cmd], shell=True)
